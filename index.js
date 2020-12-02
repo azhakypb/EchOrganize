1d90f2635271104d19bcea50ef44540ca82e6a0c
Adil_MSI <zhakypadil@gmail.com>
2020-12-02 22:23:20 +0600

    added and adjusted all functions froms sprint 1 and 2

--- index.js
+++ index.js
@@ -20,92 +20,14 @@ const LaunchRequestHandler = {
     }
 };
 
-/*  Sprint 2 DOCS */
+/* ADIL'S PART            */
 
-/*  Adding To Do List   */
-const InProgressAddToDoListIntentHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'IntentRequest' &&
-          request.intent.name === 'AddToDoListIntent' &&
-          request.dialogState !== 'COMPLETED';
-      },
-      handle(handlerInput) {
-        const currentIntent = handlerInput.requestEnvelope.request.intent;
-        return handlerInput.responseBuilder
-          .addDelegateDirective(currentIntent)
-          .getResponse();
-      }
-}
-const AddToDoListIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'AddToDoListIntent';  
-    },
-    async handle(handlerInput) {
-        const userID = handlerInput.requestEnvelope.context.System.user.userId; 
-        const todoString = handlerInput.requestEnvelope.request.intent.slots.todoString.value;
-        /*working with the date*/
-        const todoData = handlerInput.requestEnvelope.request.intent.slots.todoData.value.toString();
-        console.log(todoData);
-        /*working with the date*/
-        return todolistHelper.addToDoList(todoString, todoData, userID)
-            .then((data) => {
-                const speechText = `You have added ${todoString}  objective for ${todoData}. Say add to add another objective, or remove to remove objectives from your to do list.`;
-                return handlerInput.responseBuilder
-                    .speak(speechText)
-                    .reprompt("to do list intent handler waiting for response")
-                    .getResponse();
-            })
-            .catch((err) => {
-                console.log("Error occured while saving to do list", err);
-                return handlerInput.responseBuilder
-                    .speak("we cannot save your to do list right now. Try again!")
-                    .getResponse();
-            })
-    }
-};
-/*  Adding To Do List   */
-
-/*  Getting To Do List   */
-const GetToDoListsIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'GetToDoListsIntent';
-  },
-  async handle(handlerInput) {
-    const {responseBuilder } = handlerInput;
-    const userID = handlerInput.requestEnvelope.context.System.user.userId;
-    const todoData = handlerInput.requestEnvelope.request.intent.slots.todoData.value.toString();
-    return todolistHelper.getToDoLists(userID, todoData)
-      .then((data) => {
-        var speechText = "Your objectives are "
-        if (data.length == 0) {
-          speechText = "You do not have any objectives for this date. Say add to add an objective "
-        } else {
-          speechText += data.map(e => e.todoString).join(", ")
-        }
-        return responseBuilder
-          .speak(speechText)
-          .reprompt(GENERAL_REPROMPT)
-          .getResponse();
-      })
-      .catch((err) => {
-        const speechText = "we cannot get your to do list right now. Try again!"
-        return responseBuilder
-          .speak(speechText)
-          .getResponse();
-      })
-  }
-}
-/*  Getting To Do List   */
-
-/*  Removing objectives from To Do List   */
-const InProgressRemoveToDoListIntentHandler = {
+/*  SPRINT 3 */
+const InProgressGradeCalculatorIntentHandler = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
     return request.type === 'IntentRequest' &&
-      request.intent.name === 'RemoveToDoListIntent' &&
+      request.intent.name === 'GradeCalculatorIntent' &&
       request.dialogState !== 'COMPLETED';
   },
   handle(handlerInput) {
@@ -116,37 +38,188 @@ const InProgressRemoveToDoListIntentHandler = {
   }
 }
 
-const RemoveToDoListIntentHandler = {
+const GradeCalculatorIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'RemoveToDoListIntent';
-  }, 
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+          && handlerInput.requestEnvelope.request.intent.name === 'GradeCalculatorIntent';  
+  },
   handle(handlerInput) {
-    const {responseBuilder } = handlerInput;
-    const userID = handlerInput.requestEnvelope.context.System.user.userId; 
-    const slots = handlerInput.requestEnvelope.request.intent.slots;
-    const todoString = slots.todoString.value;
-    return todolistHelper.removeToDoList(todoString, userID)
-      .then((data) => {
-        const speechText = `You have removed objective: ${todoString}, you can remove more, by saying remove or complete`
-        return responseBuilder
-          .speak(speechText)
+      const earnedPoints = handlerInput.requestEnvelope.request.intent.slots.earnedPoints.value;
+      const totalPoints = handlerInput.requestEnvelope.request.intent.slots.totalPoints.value;
+      var percent = (earnedPoints * 100) / totalPoints;
+      percent = percent.toFixed(1);
+      var speakOutput = "error occured, try again"
+      
+      switch(true) {
+          case percent >= 97:
+              speakOutput = " it's an A plus! Great job!";
+              break;
+          case percent >= 93:
+              speakOutput = " it's a strong A! Awesome!";
+              break;
+          case percent >= 90:
+              speakOutput = " it's A minus, almost perfect.";
+              break;
+          case percent >= 87:
+              speakOutput = " it's B plus, which is good";
+              break;
+          case percent >= 83:
+              speakOutput = " it's B";
+              break;
+          case percent >= 80:
+              speakOutput = " it's B minus";
+              break;
+          case percent >= 77:
+              speakOutput = " it's C plus";
+              break;
+          case percent >= 73:
+              speakOutput = " it's a strong C, you are passing";
+              break;
+          case percent >= 70:
+              speakOutput = " it's C minus";
+              break;
+          default:
+              speakOutput = " it's below C minus. This might end up bad...";
+              break;
+      }
+      
+      
+      return handlerInput.responseBuilder
+          .speak(`It is ${percent} percent. By default ` + speakOutput)
           .reprompt(GENERAL_REPROMPT)
           .getResponse();
-      })
-      .catch((err) => {
-        const speechText = `You do not have objective: ${todoString}, you can add it by saying add`
-        return responseBuilder
-          .speak(speechText)
-          .reprompt(GENERAL_REPROMPT)
-          .getResponse();
-      })
+
   }
+  
+};
+/*  SPRINT 3 */
+
+/*  Sprint 2 DOCS */
+/*  Adding To Do List   */
+const InProgressAddToDoListIntentHandler = {
+  canHandle(handlerInput) {
+      const request = handlerInput.requestEnvelope.request;
+      return request.type === 'IntentRequest' &&
+        request.intent.name === 'AddToDoListIntent' &&
+        request.dialogState !== 'COMPLETED';
+    },
+    handle(handlerInput) {
+      const currentIntent = handlerInput.requestEnvelope.request.intent;
+      return handlerInput.responseBuilder
+        .addDelegateDirective(currentIntent)
+        .getResponse();
+    }
+}
+const AddToDoListIntentHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+          && handlerInput.requestEnvelope.request.intent.name === 'AddToDoListIntent';  
+  },
+  async handle(handlerInput) {
+      const userID = handlerInput.requestEnvelope.context.System.user.userId; 
+      const todoString = handlerInput.requestEnvelope.request.intent.slots.todoString.value;
+      /*working with the date*/
+      const todoData = handlerInput.requestEnvelope.request.intent.slots.todoData.value.toString();
+      console.log(todoData);
+      /*working with the date*/
+      return todolistHelper.addToDoList(todoString, todoData, userID)
+          .then((data) => {
+              const speechText = `You have added ${todoString}  objective for ${todoData}. Say add to add another objective, or remove to remove objectives from your to do list.`;
+              return handlerInput.responseBuilder
+                  .speak(speechText)
+                  .reprompt("to do list intent handler waiting for response")
+                  .getResponse();
+          })
+          .catch((err) => {
+              console.log("Error occured while saving to do list", err);
+              return handlerInput.responseBuilder
+                  .speak("we cannot save your to do list right now. Try again!")
+                  .getResponse();
+          })
+  }
+};
+/*  Adding To Do List   */
+
+/*  Getting To Do List   */
+const GetToDoListsIntentHandler = {
+canHandle(handlerInput) {
+  return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+    && handlerInput.requestEnvelope.request.intent.name === 'GetToDoListsIntent';
+},
+async handle(handlerInput) {
+  const {responseBuilder } = handlerInput;
+  const userID = handlerInput.requestEnvelope.context.System.user.userId;
+  const todoData = handlerInput.requestEnvelope.request.intent.slots.todoData.value.toString();
+  return todolistHelper.getToDoLists(userID, todoData)
+    .then((data) => {
+      var speechText = "Your objectives are "
+      if (data.length == 0) {
+        speechText = "You do not have any objectives for this date. Say add to add an objective "
+      } else {
+        speechText += data.map(e => e.todoString).join(", ")
+      }
+      return responseBuilder
+        .speak(speechText)
+        .reprompt(GENERAL_REPROMPT)
+        .getResponse();
+    })
+    .catch((err) => {
+      const speechText = "we cannot get your to do list right now. Try again!"
+      return responseBuilder
+        .speak(speechText)
+        .getResponse();
+    })
+}
+}
+/*  Getting To Do List   */
+
+/*  Removing objectives from To Do List   */
+const InProgressRemoveToDoListIntentHandler = {
+canHandle(handlerInput) {
+  const request = handlerInput.requestEnvelope.request;
+  return request.type === 'IntentRequest' &&
+    request.intent.name === 'RemoveToDoListIntent' &&
+    request.dialogState !== 'COMPLETED';
+},
+handle(handlerInput) {
+  const currentIntent = handlerInput.requestEnvelope.request.intent;
+  return handlerInput.responseBuilder
+    .addDelegateDirective(currentIntent)
+    .getResponse();
+}
+}
+
+const RemoveToDoListIntentHandler = {
+canHandle(handlerInput) {
+  return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+    && handlerInput.requestEnvelope.request.intent.name === 'RemoveToDoListIntent';
+}, 
+handle(handlerInput) {
+  const {responseBuilder } = handlerInput;
+  const userID = handlerInput.requestEnvelope.context.System.user.userId; 
+  const slots = handlerInput.requestEnvelope.request.intent.slots;
+  const todoString = slots.todoString.value;
+  return todolistHelper.removeToDoList(todoString, userID)
+    .then((data) => {
+      const speechText = `You have removed objective: ${todoString}, you can remove more, by saying remove or complete`
+      return responseBuilder
+        .speak(speechText)
+        .reprompt(GENERAL_REPROMPT)
+        .getResponse();
+    })
+    .catch((err) => {
+      const speechText = `You do not have objective: ${todoString}, you can add it by saying add`
+      return responseBuilder
+        .speak(speechText)
+        .reprompt(GENERAL_REPROMPT)
+        .getResponse();
+    })
+}
 }
 /*  Removing objectives from To Do List   */
 
 /*  Sprint 2 DOCS */
-
+/* ADIL'S PART            */
 const HelpIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
